5cde1c3de88d86153b2b1320afc2c2dd47454da7
Aymeric <aymericardot@gmail.com>
2025-04-24 11:00:14 +0200

    first glance of ROS motor driver

---
+++ libraries/AP_HAL_Linux/RCOutput_ROS.cpp
@@ -0,0 +1,85 @@
+#include "RCOutput_ROS.h"
+#include <rclcpp/rclcpp.hpp>
+
+#define PWM_CHAN_COUNT 16
+
+// TODO :
+// Why channel offset
+// define correctly the constructor 
+
+using namespace Linux;
+
+RCOutput_ROS::RCOutput_ROS()
+{
+    // Init ros topic or node here ?
+}
+
+RCOutput_ROS::~RCOutput_ROS()
+{
+    //Maybe delete something in ROS idk
+}
+
+void RCOutput_ROS::init()
+{
+    set_freq(0, 50);
+    // Init ros topic or node here ?
+}
+
+void RCOutput_ROS::set_freq(uint32_t chmask, uint16_t freq_hz)
+{
+    // Write the last pulse
+    for (int i=0; i<PWM_CHAN_COUNT; i++){
+        write(i, _pulse_buffer[i]);
+    }
+
+    _freq = freq_hz;
+}
+
+uint16_t RCOutput_ROS::get_freq(uint8_t ch){
+    return _freq;
+}
+
+void RCOutput_ROS::enable_ch(uint8_t ch)
+{
+    //pass
+}
+
+void RCOutput_ROS::disable_ch(uint8_t ch)
+{
+    write(ch, 0);
+}
+
+void RCOutput_ROS::write(uint8_t ch, uint16_t period_us)
+{
+    if (!_corking) {
+        _corking = true;
+        push();
+    }
+}
+
+void RCOutput_ROS::cork()
+{
+    _corking = true;
+}
+
+void RCOutput_ROS::push(){
+    if (!_corking){
+        return;
+    }
+    _corking = false;
+
+    //Push on ROS topic
+}
+
+uint16_t RCOutput_ROS::read(uint8_t chan)
+{
+    return _pulse_buffer[chan];
+}
+
+
+void RCOutput_ROS::read(uint16_t* period_us, uint8_t len)
+{
+    for (int i = 0; i < len; i++) {
+        period_us[i] = read(0 + i);
+    }
+}
---
+++ libraries/AP_HAL_Linux/RCOutput_ROS.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "AP_HAL_Linux.h"
+
+namespace Linux {
+
+class RCOutput_ROS : public AP_HAL::RCOutput {
+public :
+    RCOutput_ROS();
+    ~RCOutput_ROS();
+
+    void init() override; //OK
+
+    void set_freq(uint32_t chmask, uint16_t freq_hz) override; //OK
+    uint16_t get_freq(uint8_t ch) override; //OK
+
+    void enable_ch(uint8_t ch) override; //OK
+    void disable_ch(uint8_t ch) override; //OK
+
+    void write(uint8_t chan, uint16_t period_us) override; //OK
+    
+    void cork() override; //OK
+
+    void push() override;
+
+    uint16_t read(uint8_t chan) override;
+    void read(uint16_t* period_us, uint8_t len) override;
+
+private:
+        uint16_t _freq;
+        bool _corking;
+        uint16_t* _pulse_buffer;
+
+};
+}
